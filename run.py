a0020da98466083dce929d1165c3a24df2b877e7
Intipy <76482337+Intipy@users.noreply.github.com>
2020-12-24 18:31:22 +0900

    Update run.py

--- run.py
+++ run.py
@@ -5,7 +5,13 @@ from discord.ext import commands
 
 w = 'NzkwODQ2MTM1MzIxMjMxMzgx.X-Gi2w.wd8kpfrRxsrJVKvGISXl8ICgzq'+'U'
 bot = commands.Bot(command_prefix = '파이드 ') 
-app = discord.client()
+client = discord.client()
+
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('파이드 배워'):
+        await client.send_message(message.channel, '대답을 배웠어요! 파이드는 23시에 업데이트돼요!')
 
 
 
@@ -389,12 +395,8 @@ async def 좋아해(ctx):
 async def 반가워(ctx):
     await ctx.send('ㅇ')
     
-@app.event
-async def on_message(message)
-    if message.content.startswith('파이드 배워')
-        await app.send_message(channel, '대답을 배웠어요! 파이드는 12시에 업데이트 됩니다')
-
 
 
 
 bot.run(w)
+client.run(w)

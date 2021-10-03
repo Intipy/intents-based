efab5273bbbe92518c5408bf2a20dfac8ab7f98b
Intipy <jios6790@gmail.com>
2021-10-03 20:30:56 +0900

    Update train.py

--- train.py
+++ train.py
@@ -77,6 +77,7 @@ print("학습 데이터 생성됨")
 
 
 
+
 model = Sequential()
 model.add(Dense(200, input_shape=(len(train_x[0]),), activation='relu'))
 model.add(Dropout(0.5))
@@ -87,7 +88,7 @@ model.add(Dense(len(train_y[0]), activation='softmax'))
 sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
 model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
 
-hist = model.fit(np.array(train_x), np.array(train_y), epochs=1500, batch_size=5, verbose=1)
+hist = model.fit(np.array(train_x), np.array(train_y), epochs=60, batch_size=5, verbose=1)
 model.save('bot_model.h5', hist)
 
 print("모델 생성됨")
